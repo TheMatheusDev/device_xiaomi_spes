f674018787eeb616168f3339a3f46949aa889386
TheMatheusDev <apenasmatheus1@gmail.com>
2023-04-07 17:09:28 -0300

    spes: EvolutionX bringup

--- evolution_spes.mk
+++ evolution_spes.mk
@@ -2,14 +2,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from common AOSP configuration
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit from common EvolutionX configuration
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+EVO_BUILD_TYPE := UNOFFICIAL
 
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
 # Device identifier
-PRODUCT_NAME := aosp_spes
+PRODUCT_NAME := evolution_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11

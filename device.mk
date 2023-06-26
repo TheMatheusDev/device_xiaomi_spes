f7cb7ead9633952aef93eed6db2aa2d96263f594
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-06-26 03:09:06 +0000

    spes: Disable dynamic refreshrate
    [1] - xiaomi used a bad screen in spes, where a lot of Flicker occurs when using Dynamics RR, in the future maybe this will be fixed via software, but for now we will disable this
    
    Change-Id: Iae92b6b72a3b091e2af90092952a646573d5e804
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -61,8 +61,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.set_idle_timer_ms=500 \
-    ro.surface_flinger.set_touch_timer_ms=800 \
-    ro.surface_flinger.use_content_detection_for_refresh_rate=true
+    ro.surface_flinger.set_touch_timer_ms=800
 
 PRODUCT_VENDOR_PROPERTIES += \
     vendor.display.defer_fps_frame_count=2 \
